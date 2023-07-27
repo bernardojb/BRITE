f0ca175123e7cdcf57863a0d2fc346492a755aa7
Bernardo Braga <bernardojbraga@gmail.com>
2023-07-27 07:51:44 -0300

    deploy

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 import './globals.css'
 import * as React from 'react';
 import { Cairo } from 'next/font/google'
@@ -7,6 +7,7 @@ import { Analytics } from '@vercel/analytics/react';
 //Components
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import SmoothScroll from '@/components/SmoothScroll';
 
 //Setup & Meta
 const cairo = Cairo({ subsets: ['latin'] })
@@ -22,40 +23,13 @@ import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 export default function RootLayout({ children }) {
-  //Scroll
-  const scrollRef = React.useRef()
-  React.useEffect(() => {
-    scrollRef.current = new Lenis({
-      duration: 1.2,
-      easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      direction: "vertical",
-      gestureDirection: "vertical",
-      smooth: true,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false
-    })
-
-    scrollRef.current.on("scroll", ScrollTrigger.update)
-
-    const updateFunc = time => {
-      scrollRef.current?.raf(time * 1000)
-    }
-
-    gsap.ticker.add(updateFunc, false, true)
-
-    return () => {
-      gsap.ticker.remove(updateFunc)
-      scrollRef.current?.destroy()
-    }
-  }, [])
-  //End scroll
-
   return (
     <html lang="en">
       <body className={cairo.className}>
         <Header />
-        {children}
+        <SmoothScroll>
+          {children}
+        </SmoothScroll>
         <Footer />
         <Analytics />
       </body>
