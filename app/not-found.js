0016c57bdf6a091f28da85c9b975834b32f040d2
Bernardo Braga <bernardojbraga@gmail.com>
2023-07-28 16:35:56 -0300

    header styles

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const notFound = () => {
+    return (
+        <section className="bg-cream h-screen flex justify-center items-center">
+            <h1 className="text-xl text-brite font-semibold">
+                NÃO FOI POSSÍVEL ENCONTRAR ESSA ROTA
+            </h1>
+        </section>
+    )
+}
+
+export default notFound
